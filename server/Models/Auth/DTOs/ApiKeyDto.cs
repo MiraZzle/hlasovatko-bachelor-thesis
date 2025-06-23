f2a94e0de11edf47255316abad4cb20f52717c9e
Matěj Foukal <matfou01@gmail.com>
2025-06-23 10:35:22 +0200

    feat: add auth dtos to model

---
+++ server/Models/Auth/DTOs/ApiKeyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Models.Auth.DTOs
+{
+    public class ApiKeyDto
+    {
+        public string PartialKey { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastUsedDate { get; set; }
+    }
+}
---
+++ server/Models/Auth/DTOs/NewApiKeyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace server.Models.Auth.DTOs
+{
+    public class NewApiKeyDto
+    {
+        public string RawApiKey { get; set; } = string.Empty;
+        public ApiKeyDto KeyInfo { get; set; } = new();
+    }
+}
