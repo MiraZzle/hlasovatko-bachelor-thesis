dac11fc48dfa8de0cfa4c48dc90f434764cb4b0c
Matěj Foukal <matfou01@gmail.com>
2025-06-23 18:01:01 +0200

    feat: add session service

---
+++ server/Services/ITemplateService.cs
@@ -0,0 +1,12 @@
+﻿using server.Models.Templates.DTOs;
+
+namespace server.Services
+{
+    public interface ITemplateService
+    {
+        Task<TemplateResponseDto> CreateTemplateAsync(CreateTemplateRequestDto templateDto, Guid ownerId);
+        Task<TemplateResponseDto?> GetTemplateByIdAsync(Guid id);
+        Task<IEnumerable<TemplateResponseDto>> GetAllTemplatesForUserAsync(Guid ownerId);
+        Task<bool> DeleteTemplateAsync(Guid id, Guid ownerId);
+    }
+}
--- server/Services/TemplateService.cs
+++ server/Services/TemplateService.cs
@@ -1,6 +1,111 @@
-﻿namespace server.Services
+﻿using server.Data;
+using server.Models;
+using server.Models.Activities.DTOs;
+using server.Models.Templates;
+using server.Models.Templates.DTOs;
+using System.Text.Json;
+using server.Models.Activities;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Services
 {
-    public class TemplateService
+    public class TemplateService : ITemplateService
     {
+        private readonly AppDbContext _context;
+        private readonly IActivityService _activityService;
+
+        public TemplateService(AppDbContext context, IActivityService activityService) {
+            _context = context;
+            _activityService = activityService;
+        }
+
+        public async Task<TemplateResponseDto> CreateTemplateAsync(CreateTemplateRequestDto templateDto, Guid ownerId) {
+            var activitiesForTemplate = new List<Activity>();
+
+            foreach (var activityDto in templateDto.Activities) {
+                var createdActivityDto = await _activityService.CreateActivityAsync(activityDto);
+
+                var newActivityEntity = await _context.Activities.FindAsync(createdActivityDto.Id);
+                if (newActivityEntity != null) {
+                    activitiesForTemplate.Add(newActivityEntity);
+                }
+            }
+
+            var template = new Template {
+                OwnerId = ownerId,
+                Settings = new TemplateSettings {
+                    Title = templateDto.Title,
+                    Tags = templateDto.Tags
+                },
+
+                Definition = activitiesForTemplate
+            };
+
+            _context.Templates.Add(template);
+            await _context.SaveChangesAsync();
+
+
+            return new TemplateResponseDto {
+                Id = template.Id,
+                Title = template.Settings.Title,
+                Tags = template.Settings.Tags,
+                Definition = template.Definition.Select(a => new ActivityResponseDto {
+                    Id = a.Id,
+                    Title = a.Title,
+                    ActivityType = a.ActivityType,
+                    Definition = JsonDocument.Parse(a.Definition).RootElement
+                }).ToList()
+            };
+        }
+
+        public async Task<TemplateResponseDto?> GetTemplateByIdAsync(Guid id) {
+            var template = await _context.Templates
+               .Include(t => t.Settings)
+               .Include(t => t.Definition)
+               .AsNoTracking()
+               .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (template == null)
+                return null;
+
+            return new TemplateResponseDto {
+                Id = template.Id,
+                Title = template.Settings.Title,
+                Tags = template.Settings.Tags,
+                Definition = template.Definition.Select(a => new ActivityResponseDto {
+                    Id = a.Id,
+                    Title = a.Title,
+                    ActivityType = a.ActivityType,
+                    Definition = JsonDocument.Parse(a.Definition).RootElement
+                }).ToList()
+            };
+        }
+
+        public async Task<IEnumerable<TemplateResponseDto>> GetAllTemplatesForUserAsync(Guid ownerId) {
+            var templates = await _context.Templates
+                .Include(t => t.Settings)
+                .Where(t => t.OwnerId == ownerId)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return templates.Select(template => new TemplateResponseDto {
+                Id = template.Id,
+                Title = template.Settings.Title,
+                Tags = template.Settings.Tags
+            });
+        }
+
+        public async Task<bool> DeleteTemplateAsync(Guid id, Guid ownerId) {
+            var template = await _context.Templates
+                .Include(t => t.Definition)
+                .FirstOrDefaultAsync(t => t.Id == id && t.OwnerId == ownerId);
+
+            if (template == null)
+                return false;
+
+            _context.Templates.Remove(template);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
