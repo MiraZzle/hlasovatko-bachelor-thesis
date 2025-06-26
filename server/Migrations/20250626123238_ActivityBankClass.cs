3542cc67d442a3abb547b49d8494ca95de330a6a
Matěj Foukal <matfou01@gmail.com>
2025-06-26 15:08:05 +0200

    chore: migrate to new model

---
+++ server/Migrations/20250626123238_ActivityBankClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivityBankClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Users_OwnerId",
+                table: "Activities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Activities_OwnerId",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Activities");
+
+            migrationBuilder.CreateTable(
+                name: "BankActivities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ActivityType = table.Column<string>(type: "text", nullable: false),
+                    Definition = table.Column<string>(type: "jsonb", nullable: false),
+                    Tags = table.Column<List<string>>(type: "text[]", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankActivities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankActivities_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankActivities_OwnerId",
+                table: "BankActivities",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankActivities");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Activities",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_OwnerId",
+                table: "Activities",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Users_OwnerId",
+                table: "Activities",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
