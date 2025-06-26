2ed84ff0975ed7057de3b9e02d9cf3347e5e900e
Matěj Foukal <matfou01@gmail.com>
2025-06-26 21:50:20 +0200

    chore: migrate to new session model

---
+++ server/Migrations/20250626181126_UpdateModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TemplateVersion",
+                table: "Sessions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TemplateVersion",
+                table: "Sessions");
+        }
+    }
+}
