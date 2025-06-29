fe99f7cdaa92ac4f8887d4b44c62550ab6d5f3fe
Matěj Foukal <matfou01@gmail.com>
2025-06-29 22:43:04 +0200

    feat: add statistics model

---
+++ server/Models/Statistics/DTOs/StatisticsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Models.Statistics.DTOs
+{
+    public class StatisticsDto
+    {
+        public int TotalSessions { get; set; }
+        public int TotalActivities { get; set; }
+        public string MostCommonActivityType { get; set; } = "N/A";
+    }
+}
