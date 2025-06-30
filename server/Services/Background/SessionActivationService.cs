1acce467cb6027911abc2e3c80db4fc907d4d2d9
Matěj Foukal <matfou01@gmail.com>
2025-06-30 14:29:03 +0200

    feat: add bg service for planned sessions

---
+++ server/Services/Background/SessionActivationService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Models.Enums;
+
+namespace server.Services.Background
+{
+    public class SessionActivationService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<SessionActivationService> _logger;
+        private readonly short _ActivationHourInterval = 2; // Reasonable -> can be adjusted later
+
+        public SessionActivationService(IServiceProvider serviceProvider, ILogger<SessionActivationService> logger) {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+            while (!stoppingToken.IsCancellationRequested) {
+                try {
+                    await ActivatePlannedSessions();
+                }
+                catch (Exception ex) {
+                    _logger.LogError(ex, "An error occurred while activating planned sessions");
+                }
+
+                await Task.Delay(TimeSpan.FromHours(_ActivationHourInterval), stoppingToken);
+            }
+        }
+
+        private async Task ActivatePlannedSessions() {
+            using (var scope = _serviceProvider.CreateScope()) {
+                _logger.LogInformation("Checking for planned sessions to activate...");
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var sessionService = scope.ServiceProvider.GetRequiredService<ISessionService>();
+
+                // Get planned sessions that are due for activation
+                var sessionsToActivate = await dbContext.Sessions
+                    .Where(s => s.Status == SessionStatus.Planned && s.ActivationDate.HasValue && s.ActivationDate.Value <= DateTime.UtcNow)
+                    .Include(s => s.Template)
+                    .ToListAsync();
+
+                if (sessionsToActivate.Any()) {
+                    _logger.LogInformation($"Found {sessionsToActivate.Count} sessions to activate");
+
+                    foreach (var session in sessionsToActivate) {
+                        try {
+                            await sessionService.StartSessionAsync(session.Id, session.Template.OwnerId);
+                        }
+                        catch (Exception ex) {
+                            _logger.LogError(ex, $"Failed to activate session {session.Id}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- server/Services/SessionService.cs
+++ server/Services/SessionService.cs
@@ -70,14 +70,18 @@ namespace server.Services
                 throw new Exception("Template not found or you do not have permission to access it.");
             }
 
+            // check if session is planned and format the date
+            DateTime? setDate = request.ActivationDate.HasValue
+                ? DateTime.SpecifyKind(request.ActivationDate.Value, DateTimeKind.Utc)
+                : null;
+
             var session = new Session {
                 Title = !string.IsNullOrEmpty(request.Title) ? request.Title : template.Settings.Title,
                 TemplateId = template.Id,
                 TemplateVersion = template.Version,
                 Status = request.ActivationDate.HasValue ? SessionStatus.Planned : SessionStatus.Inactive,
                 JoinCode = await GenerateUniqueJoinCode(),
-                ActivationDate = request.ActivationDate,
-                Mode = request.Mode ?? SessionMode.TeacherPaced,
+                ActivationDate = setDate,
                 CurrentActivity = null
             };
 
@@ -196,6 +200,7 @@ namespace server.Services
                 Mode = session.Mode,
                 Participants = session.Participants,
                 CurrentActivity = session.CurrentActivity,
+                CreatedAt = session.Created,
                 Activities = session.Activities?.Select(a => new ActivityResponseDto {
                     Id = a.Id,
                     Title = a.Title,
