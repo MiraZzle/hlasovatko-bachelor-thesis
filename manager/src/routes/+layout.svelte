4fee54b8f830de66d43749efc19125a31e3d98ee
Matěj Foukal <matfou01@gmail.com>
2025-07-02 10:56:31 +0200

    feat: add toaster component

--- manager/src/routes/+layout.svelte
+++ manager/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '$styles/global.scss';
+	import Toaster from '$components/elements/typography/Toaster.svelte';
 </script>
+
+<Toaster />
 
 <div id="page-content">
 	<slot />
