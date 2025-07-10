2ad7f7ce3e326d32c3d79393ca4ccbe46c4e8922
Matěj Foukal <matfou01@gmail.com>
2025-07-10 17:57:35 +0200

    feat: add shell util for registering users

---
+++ register_user.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# API URL for registration endpoint
+API_URL="http://localhost/server/api/v1/auth/register"
+
+# Display usage information for the script
+show_help() {
+  cat << EOF
+Usage: $0 "User Name" <email> <password>
+Registers a new user for the EngaGenie platform by sending a request to the API.
+
+Arguments:
+  "User Name"   The full name of the user (must be enclosed in quotes).
+  email         The user's email address.
+  password      The user's password (at least 6 chars).
+
+Options:
+  -h, --help    Display this help message and exit.
+
+Example:
+  $0 "Clovek Uzasny" "clovek.uzasny@example.com" "secretPassword123"
+EOF
+}
+
+# Check for help flag or incorrect number of args
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  show_help
+  exit 0
+fi
+
+if [ "$#" -ne 3 ]; then
+  show_help
+  exit 1
+fi
+
+NAME=$1
+EMAIL=$2
+PASSWORD=$3
+
+JSON_PAYLOAD=$(cat <<EOF
+{
+  "Name": "$NAME",
+  "Email": "$EMAIL",
+  "Password": "$PASSWORD"
+}
+EOF
+)
+
+# Send the curl request
+echo "Trying to register user: $NAME <$EMAIL>"
+curl -s -X POST \
+     -H "Content-Type: application/json" \
+     -d "$JSON_PAYLOAD" \
+     "$API_URL"
+
+echo
